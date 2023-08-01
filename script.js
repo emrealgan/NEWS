ae32c35c3a6f090d438cd005bdd30f6e6a196a2f
emrealgan <139621618+emrealgan@users.noreply.github.com>
2023-08-01 16:32:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// api key = e68c7282fb76449fbd24dcc2e5e44551
+const newsList = document.querySelector(".news-list");
+
+function getData(url)
+{
+    fetch(url)
+    .then( res => res.json() )
+    .then( data => createNews(data.articles) )
+    .catch( err => console.log(err) )
+}
+
+getData("https://newsapi.org/v2/top-headlines?country=tr&apiKey=e68c7282fb76449fbd24dcc2e5e44551");
+
+function createNews(data)
+{   
+    for(let i = 0; i < 20; i++)
+    {   
+
+        let newsItem = document.createElement("li");
+        newsItem.classList.add("news-item");
+        
+        let newsBody = document.createElement("div");
+        newsBody.classList.add("news-body");
+
+        let newsTitle = document.createElement("strong");
+        newsTitle.classList.add("news-title");
+
+        let newsButton = document.createElement("a");
+        newsButton.classList.add("news-button");
+        newsButton.target = "blank";
+
+        newsList.appendChild(newsItem);
+        newsItem.appendChild(newsBody)
+        newsItem.appendChild(newsButton);
+        newsButton.appendChild(newsTitle);
+
+        const maxLength = 120;
+        const title = data[i].title.length > maxLength ? data[i].title.substring(0, maxLength) + "..." : data[i].title;
+    
+        newsTitle.innerHTML = title;
+        newsButton.href = data[i].url;
+    }
+}
